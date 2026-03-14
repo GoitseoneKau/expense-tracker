10d23f05a35aeb74831fd3f03367eeab47083c80
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2026-03-15 00:43:28 +0200

    feat: implement expense tracker component with chart visualization
    - Added ExpenseTrackerComponent for managing expenses and income.
    - Integrated Chart.js for visual representation of expenses.
    - Updated app.component.ts to include ExpenseTrackerComponent.
    - Enhanced app.config.ts for better configuration management.
    - Modified index.html to include Google Fonts for improved typography.
    - Added global styles in styles.css for consistent layout and design.
    - Created expense-tracker.component.html and expense-tracker.component.css for component-specific styling and structure.
    - Implemented unit tests for ExpenseTrackerComponent to ensure functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseTrackerComponent } from './expense-tracker/expense-tracker.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ExpenseTrackerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'expense-tracker';
+  title = 'Expense-Tracker';
 }
